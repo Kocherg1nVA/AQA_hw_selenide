b173ff314bda3f172a4f8d0560fe84c6e8830bd1
Vadim Kochergin <kochergin1990vadim@gmail.com>
2025-02-05 18:48:39 +0300

    task2 test added

--- src/test/java/Task2CardDeliveryTest.java
+++ src/test/java/Task2CardDeliveryTest.java
@@ -13,19 +13,13 @@ public class Task2CardDeliveryTest {
 
     Calendar calendar = new GregorianCalendar();
     int daysOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-//    int currentDay = calendar.get(Calendar.DATE);
-
-//    public int meetingDayService(int days){
-//        return currentDay + days;
-//    }
 
     @Test
-    public void shouldSendFormThisMonth() {
+    public void shouldSendFormNextWeekMeeting() {
 
         String planDate = service.planningDate(7, "dd.MM.yyyy");
         String planDay = service.planningDate(7, "d");
 
-//        int resultDay = meetingDayService(7);
         int resultDay = calendar.get(Calendar.DAY_OF_WEEK);
 
         Selenide.open("http://localhost:9999");
@@ -48,33 +42,4 @@ public class Task2CardDeliveryTest {
         $("[data-test-id='notification'] .notification__content").shouldBe(Condition.text("Встреча успешно " +
                 "забронирована на " + planDate));
     }
-
-//    @Test
-//    public void shouldSendFormNextMonth() {
-//
-//        String planDate = service.planningDate(25, "dd.MM.yyyy");
-//        String planDay = service.planningDate(25, "d");
-//
-//        int resultDay = meetingDayService(25);
-//
-//        Selenide.open("http://localhost:9999");
-//
-//        $("[data-test-id='city'] input").setValue("ка");
-//        $$(".menu-item").find(Condition.text("Йошкар-Ола")).click();
-//        $(".icon-button .icon_name_calendar").click();
-//        if (resultDay > daysOfMonth) {
-//            $("[role='button'][data-step='1'].calendar__arrow_direction_right").click();
-//            $$(".calendar__day").find(Condition.text(planDay)).click();
-//        } else {
-//            $$(".calendar__day").find(Condition.text(planDay)).click();
-//        }
-//        $("[data-test-id='name'] input").setValue("Иванов Иван");
-//        $("[data-test-id='phone'] input").setValue("+79991251212");
-//        $("[data-test-id='agreement']").click();
-//        $$("button").find(Condition.text("Забронировать")).click();
-//        $("[data-test-id='notification'] .notification__title").shouldBe(Condition.text("Успешно!"),
-//                Duration.ofSeconds(15));
-//        $("[data-test-id='notification'] .notification__content").shouldBe(Condition.text("Встреча успешно " +
-//                "забронирована на " + planDate));
-//    }
 }
